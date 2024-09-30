56f354e24fac5798e0a647f33d0e3a5ab6ba2986
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-09-30 21:14:15 +0100

    Create 8-getBudgetCurrentYear.js

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,14 @@
+function getCurrentYear() {
+  return (new Date()).getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const currentYear = getCurrentYear();
+
+  return {
+    [`income-${currentYear}`]: income,
+    [`gdp-${currentYear}`]: gdp,
+    [`capita-${currentYear}`]: capita,
+  };
+
+}
