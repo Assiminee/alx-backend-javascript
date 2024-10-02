0b4a7c5881f443e10754dd53df6d7889194f93b0
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-10-02 21:59:24 +0100

    task3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,3 +1,12 @@
-export default function handleProfileSignup() {
+import { uploadPhoto, createUser } from './utils';
 
+export default async function handleProfileSignup() {
+  try {
+    const resp1 = await uploadPhoto();
+    const resp2 = await createUser();
+
+    console.log(`${resp1.body} ${resp2.firstName} ${resp2.lastName}`);
+  } catch (error) {
+    console.error('Signup system offline');
+  }
 }
