919c7a3bde2326d169509f564a4f6eb276408840
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-10-02 23:08:06 +0100

    task8

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line consistent-return
+export default function divideFunction(numerator, denominator) {
+  if (!denominator) throw Error('cannot divide by 0');
+
+  return numerator / denominator;
+}
