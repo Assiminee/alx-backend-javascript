9949add0fab79726aa8f918b0c0a5629230284d6
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-10-02 22:42:02 +0100

    task6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,16 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((resp) => resp);
+    .then((resp) => {
+      const responses = [];
+
+      for (const response of resp) {
+        if (response.status !== 'fulfilled') {
+          response.value = response.reason;
+          delete response.reason;
+        }
+        responses.push(response);
+      }
+      return responses;
+    });
 }
