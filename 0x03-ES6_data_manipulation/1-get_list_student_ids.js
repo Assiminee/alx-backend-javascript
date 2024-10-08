e5c5304d788c64e380d4f93058775b6d587e420c
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-10-09 00:18:39 +0100

    task1

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+const getListStudentIds = (studentList) => {
+  if (!Array.isArray(studentList)) return [];
+
+  return studentList.map((student) => (student.id));
+};
+
+export default getListStudentIds;
