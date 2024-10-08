0033c41db6243d44752d7b03fd48fa1e0f6bb6ef
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-10-09 00:53:41 +0100

    task5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,12 @@
-const createInt8TypedArray = () => {
+const createInt8TypedArray = (length, position, value) => {
+  if (position < 0 || position >= length) throw Error('Position outside range');
 
-}
+  const int8buff = new ArrayBuffer(length);
+  const array8 = new Int8Array(int8buff);
+
+  array8[position] = value;
+
+  return new DataView(int8buff);
+};
 
 export default createInt8TypedArray;
