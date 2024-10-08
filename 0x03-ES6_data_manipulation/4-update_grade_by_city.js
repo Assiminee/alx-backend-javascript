b0331a1e188e5abf3d3ecead8006dcf0a373b1b5
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-10-09 00:49:30 +0100

    edit

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,9 @@
 const updateStudentGradeByCity = (studentList, location, newGrades) => studentList.filter(
   (student) => (student.location.toUpperCase() === location.toUpperCase()),
 ).map((student) => {
-  const grade = newGrades.find((grade) => grade.studentId === student.id);
+  const newGrade = newGrades.find((grade) => grade.studentId === student.id);
   // eslint-disable-next-line no-param-reassign
-  student.grade = grade || 'N/A';
+  student.grade = newGrade ? newGrade.grade : 'N/A';
   return student;
 });
 
