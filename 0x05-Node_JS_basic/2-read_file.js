898255c095d6425b3ab8213054fa0c141906c104
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-11-26 21:59:30 +0100

    reading db

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+const countStudents = (path) => {
+    try {
+        const data = fs.readFileSync(path, 'utf8');
+        const fields = {}
+        const students = data.split('\n')
+            .filter(student => student.length !== 0);
+
+        students.shift();
+
+        console.log(`Number of students: ${students.length}`);
+
+        for (const student of students) {
+            let line = student.trim().split(',');
+            let field = line[line.length - 1];
+
+            if (!(field in fields)) {
+                fields[field] = {count: 1, names: [line[0]]};
+            } else {
+                fields[field].count += 1;
+                fields[field].names.push(line[0]);
+            }
+        }
+
+        for (const [field, values] of Object.entries(fields)) {
+            console.log(`Number of students in ${field}: ${values.count}. List: ${values.names.join(', ')}`);
+        }
+
+
+    } catch (e) {
+        console.error('Cannot load the database');
+    }
+}
+
+module.exports = countStudents;
