c26df94de35e34332a064c33276436014cc4df70
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-11-26 21:10:13 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,22 +3,17 @@ const stdout_fd = process.stdout;
 
 stdout_fd.write("Welcome to Holberton School, what is your name?\n");
 
+stdin_fd.on('readable', () => {
+    let chunk = stdin_fd.read();
+
+    if (chunk !== null) {
+        stdout_fd.write(`Your name is ${chunk}\n`);
+    }
+});
+
 if (!stdin_fd.isTTY) {
-    stdin_fd.on('readable', () => {
-        let chunk;
-
-        while ((chunk = stdin_fd.read()) !== null) {
-            stdout_fd.write(`Your name is ${chunk}\n`);
-        }
-    });
-
     stdin_fd.on('end', () => {
         stdout_fd.write("This important software is now closing\n");
         stdout_fd.end();
     })
-} else {
-    stdin_fd.on('readable', () => {
-        let chunk = stdin_fd.read();
-        stdout_fd.write(`Your name is ${chunk}\n`);
-    });
 }
