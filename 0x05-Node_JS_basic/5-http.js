bd687ac5b071aec8b38041c181dd83accd01e514
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-11-27 01:36:24 +0100

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,40 +3,47 @@ const fs = require('fs');
 
 const db = process.argv[2];
 
-const countStudents = (path) => new Promise((resolve, reject) => {
-  fs.readFile(path, 'utf-8', (err, data) => {
-    if (err) {
-      reject(new Error('Cannot load the database'));
-      return;
-    }
-    let message = '';
-    const fields = {};
-    const students = data.split('\n')
-      .map((student) => student.trim())
-      .filter((student) => student.length !== 0);
-
-    students.shift();
-
-    message += `\nNumber of students: ${students.length}`;
-
-    for (const student of students) {
-      const line = student.split(',');
-      const field = line[line.length - 1];
-
-      if (!(field in fields)) {
-        fields[field] = { count: 1, names: [line[0]] };
-      } else {
-        fields[field].count += 1;
-        fields[field].names.push(line[0]);
+async function countStudents(dbName) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(dbName, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
       }
-    }
 
-    for (const [field, values] of Object.entries(fields)) {
-      message += `\nNumber of students in ${field}: ${values.count}. List: ${values.names.join(', ')}`;
-    }
-    resolve(message);
+      const stdList = data.split('\n').slice(1, -1);
+      const ldb = [];
+      const fields = {};
+      for (const l of stdList) {
+        const temp = l.split(',');
+        ldb.push(temp);
+        fields[temp[temp.length - 1]] = [];
+      }
+      for (const s of ldb) {
+        if (s[s.length - 1] === 'CS') fields.CS.push(s[0]);
+        else if (s[s.length - 1] === 'SWE') fields.SWE.push(s[0]);
+      }
+      const info = {
+        count: stdList.length,
+        cs: {
+          count: fields.CS.length,
+          names: fields.CS.join(', '),
+        },
+        swe: {
+          count: fields.SWE.length,
+          names: fields.SWE.join(', '),
+        },
+      };
+      const msg = [
+        `Number of students: ${info.count}`,
+        `Number of students in CS: ${info.cs.count}. List: ${info.cs.names}`,
+        `Number of students in SWE: ${info.swe.count}. List: ${info.swe.names}`,
+      ];
+
+      resolve(msg.join('\n'));
+    });
   });
-});
+}
 
 const app = createServer((req, res) => {
   if (req.url === '/') {
